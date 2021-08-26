ce414e0ee3b644b3d91ad6f9f6533776977c4823
maxschr90 <35767016+maxschr90@users.noreply.github.com>
2021-08-26 09:17:25 +0100

    update

--- Code/est_parms_bpareto.m
+++ Code/est_parms_bpareto.m
@@ -10,15 +10,13 @@ if max(isempty(dmax),isempty(dmin)) == 0
     problem = createOptimProblem('fmincon', 'objective',@(y)bpareto_ll(y,x,dmax,dmin), 'lb',[0,0], 'ub', [2,10000], 'x0',[0,2.5], 'options', options);
     [theta_hat,Fval] = run(ms,problem,1000);
     alpha = 1./(theta_hat(2)*exp(-t*theta_hat(1)));
-    SE = sqrt(diag(inv(hessian(@(z)bpareto_ll(z,x,dmax,dmin),[theta_hat]))));
-    Calibrated_Model = struct('Model', name,'lambda', theta_hat(1),'eta_zero', theta_hat(2), 'SE', SE, 'alpha', alpha, 'dmin',dmin, 'dmax', dmax, 'LL',-Fval, 'Estimated_Bounds',0,'Problem', problem);
+    Calibrated_Model = struct('Model', name,'lambda', theta_hat(1),'eta_zero', theta_hat(2), 'alpha', alpha, 'dmin',dmin, 'dmax', dmax, 'LL',-Fval, 'Estimated_Bounds',0,'Problem', problem);
 
 else
     problem = createOptimProblem('fmincon', 'objective',@(y)bpareto_ll_v2(y,x), 'lb',[0,0, max(x),0], 'ub', [2,1000000,10000000, min(x)], 'x0',[0, 2.5, max(x), min(x)], 'options', options);
     [theta_hat,Fval] = run(ms,problem,1000);
     alpha = 1./(theta_hat(2)*exp(-t*theta_hat(1)));
-    SE = sqrt(diag(inv(hessian(@(z)bpareto_ll_v2(z,x),[theta_hat]))));
-    Calibrated_Model = struct('Model', name,'lambda', theta_hat(1),'eta_zero', theta_hat(2), 'SE', SE, 'alpha', alpha, 'dmin',theta_hat(4), 'dmax', theta_hat(3), 'LL',-Fval, 'Estimated_Bounds',1,'Problem', problem);
+    Calibrated_Model = struct('Model', name,'lambda', theta_hat(1),'eta_zero', theta_hat(2), 'alpha', alpha, 'dmin',theta_hat(4), 'dmax', theta_hat(3), 'LL',-Fval, 'Estimated_Bounds',1,'Problem', problem);
 end
 
 end

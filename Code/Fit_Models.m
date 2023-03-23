dd9967f4e368416209e7e2e391cdb8f1cce970b0
maxschr90 <35767016+maxschr90@users.noreply.github.com>
2023-03-23 16:09:26 +0000

    edits

--- Code/Fit_Models.m
+++ Code/Fit_Models.m
@@ -7,172 +7,172 @@ close all
 Load_Data % Loads data for Cities cities from excel file
 
 %%% 2. Estimate Base Model
-    dmax_Cities = (max(Influenza_Cities(24:end,2:end)));
-    dmin_Cities = (min(Influenza_Cities(24:end,2:end)));
-    for n = 1:8
-        [Base_Model(n)] = est_parms_bpareto(Influenza_Cities(26:end,n+1),dmax_Cities(n),dmin_Cities(n), citynames(n));
-    end
-        dmax_US = max(Influenza_US(19:end,2)');
-        dmin_US = min(Influenza_US(19:end,2)');
-        [Base_Model(n+1)] = est_parms_bpareto(Influenza_US(21:end,2),dmax_US,dmin_US, 'US');
+dmax_Cities = (max(Influenza_Cities(24:end,2:end)));
+dmin_Cities = (min(Influenza_Cities(24:end,2:end)));
+for n = 1:8
+    [Base_Model(n)] = est_parms_bpareto(Influenza_Cities(26:end,n+1),dmax_Cities(n),dmin_Cities(n), citynames(n));
+end
+dmax_US = max(Influenza_US(19:end,2)');
+dmin_US = min(Influenza_US(19:end,2)');
+[Base_Model(n+1)] = est_parms_bpareto(Influenza_US(21:end,2),dmax_US,dmin_US, 'US');
 
-        dmax_EW_1848 = max(Influenza_EW(10:52,2)');
-        dmin_EW_1848 = min(Influenza_EW(10:52,2)');
-        [Base_Model(n+2)] = est_parms_bpareto(Influenza_EW(12:52,2),dmax_EW_1848,dmin_EW_1848, 'England&Wales 1848');
+dmax_EW_1848 = max(Influenza_EW(10:52,2)');
+dmin_EW_1848 = min(Influenza_EW(10:52,2)');
+[Base_Model(n+2)] = est_parms_bpareto(Influenza_EW(12:52,2),dmax_EW_1848,dmin_EW_1848, 'England&Wales 1848');
 
-        dmax_EW_1890 = max(Influenza_EW(53:80,2)');
-        dmin_EW_1890 = min(Influenza_EW(53:80,2)');
-        [Base_Model(n+3)] = est_parms_bpareto(Influenza_EW(55:80,2),dmax_EW_1890,dmin_EW_1890, 'England&Wales 1890');
+dmax_EW_1890 = max(Influenza_EW(53:80,2)');
+dmin_EW_1890 = min(Influenza_EW(53:80,2)');
+[Base_Model(n+3)] = est_parms_bpareto(Influenza_EW(55:80,2),dmax_EW_1890,dmin_EW_1890, 'England&Wales 1890');
 
-        dmax_EW_1918 = max(Influenza_EW(81:119,2)');
-        dmin_EW_1918 = min(Influenza_EW(81:119,2)');
-        [Base_Model(n+4)] = est_parms_bpareto(Influenza_EW(83:119,2),dmax_EW_1918,dmin_EW_1918, 'England&Wales 1918');
+dmax_EW_1918 = max(Influenza_EW(81:119,2)');
+dmin_EW_1918 = min(Influenza_EW(81:119,2)');
+[Base_Model(n+4)] = est_parms_bpareto(Influenza_EW(83:119,2),dmax_EW_1918,dmin_EW_1918, 'England&Wales 1918');
 
-        dmax_EW_1957 = max(Influenza_EW(120:130,2)');
-        dmin_EW_1957 = min(Influenza_EW(120:130,2)');
-        [Base_Model(n+5)] = est_parms_bpareto(Influenza_EW(123:130,2),dmax_EW_1957,dmin_EW_1957, 'England&Wales 1957');
+dmax_EW_1957 = max(Influenza_EW(120:130,2)');
+dmin_EW_1957 = min(Influenza_EW(120:130,2)');
+[Base_Model(n+5)] = est_parms_bpareto(Influenza_EW(123:130,2),dmax_EW_1957,dmin_EW_1957, 'England&Wales 1957');
 
-        dmax_EW_1968 = max(Influenza_EW(131:end,2)');
-        dmin_EW_1968 = min(Influenza_EW(131:end,2)');
-        [Base_Model(n+6)] = est_parms_bpareto(Influenza_EW(134:end,2),dmax_EW_1968,dmin_EW_1968, 'England&Wales 1968');
-        
-        Robustness_Checks
+dmax_EW_1968 = max(Influenza_EW(131:end,2)');
+dmin_EW_1968 = min(Influenza_EW(131:end,2)');
+[Base_Model(n+6)] = est_parms_bpareto(Influenza_EW(134:end,2),dmax_EW_1968,dmin_EW_1968, 'England&Wales 1968');
+
+Robustness_Checks
 %%% 3. Estimate Model with common Bounds
-    dmax_Cities = (max(dmax_Cities));
-    dmin_Cities = (min(dmin_Cities));
-    for n = 1:8
-        [Common_Bounds_Model(n)] = est_parms_bpareto(Influenza_Cities(26:end,n+1),dmax_Cities,dmin_Cities, citynames(n));
-    end
-        
+dmax_Cities = (max(dmax_Cities));
+dmin_Cities = (min(dmin_Cities));
+for n = 1:8
+    [Common_Bounds_Model(n)] = est_parms_bpareto(Influenza_Cities(26:end,n+1),dmax_Cities,dmin_Cities, citynames(n));
+end
+
 %%% 4. Estimate Model with theoretical Bounds
 
-    for n = 1:8
-        [Theoretical_Bounds_Model(n)] = est_parms_bpareto(Influenza_Cities(26:end,n+1),1000000,1, citynames(n));
-    end
+for n = 1:8
+    [Theoretical_Bounds_Model(n)] = est_parms_bpareto(Influenza_Cities(26:end,n+1),1000000,1, citynames(n));
+end
 
-        [Theoretical_Bounds_Model(n+1)] = est_parms_bpareto(Influenza_US(21:end,2),1000000,1, 'US');
+[Theoretical_Bounds_Model(n+1)] = est_parms_bpareto(Influenza_US(21:end,2),1000000,1, 'US');
 
-        [Theoretical_Bounds_Model(n+2)] = est_parms_bpareto(Influenza_EW(12:52,2),1000000,1, 'England&Wales 1848');
+[Theoretical_Bounds_Model(n+2)] = est_parms_bpareto(Influenza_EW(12:52,2),1000000,1, 'England&Wales 1848');
 
-        [Theoretical_Bounds_Model(n+3)] = est_parms_bpareto(Influenza_EW(55:80,2),1000000,1, 'England&Wales 1890');
+[Theoretical_Bounds_Model(n+3)] = est_parms_bpareto(Influenza_EW(55:80,2),1000000,1, 'England&Wales 1890');
 
-        [Theoretical_Bounds_Model(n+4)] = est_parms_bpareto(Influenza_EW(83:119,2),1000000,1, 'England&Wales 1918');
+[Theoretical_Bounds_Model(n+4)] = est_parms_bpareto(Influenza_EW(83:119,2),1000000,1, 'England&Wales 1918');
 
-        [Theoretical_Bounds_Model(n+5)] = est_parms_bpareto(Influenza_EW(123:130,2),1000000,1, 'England&Wales 1957');
+[Theoretical_Bounds_Model(n+5)] = est_parms_bpareto(Influenza_EW(123:130,2),1000000,1, 'England&Wales 1957');
 
-        [Theoretical_Bounds_Model(n+6)] = est_parms_bpareto(Influenza_EW(134:end,2),1000000,1, 'England&Wales 1968');   
-       
-        
- %%% 5. Estimate Model with estimated Bounds
+[Theoretical_Bounds_Model(n+6)] = est_parms_bpareto(Influenza_EW(134:end,2),1000000,1, 'England&Wales 1968');
 
-    for n = 1:8
-        [Estimated_Bounds_Model(n)] = est_parms_bpareto(Influenza_Cities(24:end,n+1),[],[], citynames(n));
-    end
-        [Estimated_Bounds_Model(n+1)] = est_parms_bpareto(Influenza_US(19:end,2),[],[], 'US');
 
-        [Estimated_Bounds_Model(n+2)] = est_parms_bpareto(Influenza_EW(11:52,2),[],[], 'England&Wales 1848');
+%%% 5. Estimate Model with estimated Bounds
 
-        [Estimated_Bounds_Model(n+3)] = est_parms_bpareto(Influenza_EW(53:80,2),[],[], 'England&Wales 1890');
+for n = 1:8
+    [Estimated_Bounds_Model(n)] = est_parms_bpareto(Influenza_Cities(24:end,n+1),[],[], citynames(n));
+end
+[Estimated_Bounds_Model(n+1)] = est_parms_bpareto(Influenza_US(19:end,2),[],[], 'US');
 
-        [Estimated_Bounds_Model(n+4)] = est_parms_bpareto(Influenza_EW(81:119,2),[],[], 'England&Wales 1918');
+[Estimated_Bounds_Model(n+2)] = est_parms_bpareto(Influenza_EW(11:52,2),[],[], 'England&Wales 1848');
 
-        [Estimated_Bounds_Model(n+5)] = est_parms_bpareto(Influenza_EW(120:130,2),[],[], 'England&Wales 1957');
+[Estimated_Bounds_Model(n+3)] = est_parms_bpareto(Influenza_EW(53:80,2),[],[], 'England&Wales 1890');
 
-        [Estimated_Bounds_Model(n+6)] = est_parms_bpareto(Influenza_EW(131:end,2),[],[], 'England&Wales 1968');
-        
-           
-%%% 6. Estimate Weibull Model 
+[Estimated_Bounds_Model(n+4)] = est_parms_bpareto(Influenza_EW(81:119,2),[],[], 'England&Wales 1918');
+
+[Estimated_Bounds_Model(n+5)] = est_parms_bpareto(Influenza_EW(120:130,2),[],[], 'England&Wales 1957');
+
+[Estimated_Bounds_Model(n+6)] = est_parms_bpareto(Influenza_EW(131:end,2),[],[], 'England&Wales 1968');
+
+
+%%% 6. Estimate Weibull Model
 %%% Deaths need to be rescaled for Weibull Distribution
-    for n = 1:8
-        [Weibull_Model(n)] = est_parms_weibull(Influenza_Cities(26:end,n+1)/10, citynames(n));
-    end
-        [Weibull_Model(n+1)] = est_parms_weibull(Influenza_US(21:end,2)/10, 'US');
+for n = 1:8
+    [Weibull_Model(n)] = est_parms_weibull(Influenza_Cities(26:end,n+1)/10, citynames(n));
+end
+[Weibull_Model(n+1)] = est_parms_weibull(Influenza_US(21:end,2)/10, 'US');
 
-        [Weibull_Model(n+2)] = est_parms_weibull(Influenza_EW(12:52,2)/10, 'England&Wales 1848');
+[Weibull_Model(n+2)] = est_parms_weibull(Influenza_EW(12:52,2)/10, 'England&Wales 1848');
 
-        [Weibull_Model(n+3)] = est_parms_weibull(Influenza_EW(55:80,2)/10, 'England&Wales 1890');
+[Weibull_Model(n+3)] = est_parms_weibull(Influenza_EW(55:80,2)/10, 'England&Wales 1890');
 
-        [Weibull_Model(n+4)] = est_parms_weibull(Influenza_EW(83:119,2)/10, 'England&Wales 1918');
+[Weibull_Model(n+4)] = est_parms_weibull(Influenza_EW(83:119,2)/10, 'England&Wales 1918');
 
-        [Weibull_Model(n+5)] = est_parms_weibull(Influenza_EW(123:130,2)/10, 'England&Wales 1957');
+[Weibull_Model(n+5)] = est_parms_weibull(Influenza_EW(123:130,2)/10, 'England&Wales 1957');
 
-        [Weibull_Model(n+6)] = est_parms_weibull(Influenza_EW(134:end,2)/10, 'England&Wales 1968');         
+[Weibull_Model(n+6)] = est_parms_weibull(Influenza_EW(134:end,2)/10, 'England&Wales 1968');
 
 
 %%% 7. Model Summary Tables
 % Base Model
-   Obs = [sum(Influenza_Cities(26:end,2:9)==Influenza_Cities(26:end,2:9)), sum(Influenza_US(21:end,2)==Influenza_US(21:end,2)),sum(Influenza_EW(12:52,2)==Influenza_EW(12:52,2)),sum(Influenza_EW(55:80,2)==Influenza_EW(55:80,2)),sum(Influenza_EW(83:119,2)==Influenza_EW(83:119,2)),sum(Influenza_EW(123:130,2)==Influenza_EW(123:130,2)),sum(Influenza_EW(133:end,2)==Influenza_EW(133:end,2))];
+Obs = [sum(Influenza_Cities(26:end,2:9)==Influenza_Cities(26:end,2:9)), sum(Influenza_US(21:end,2)==Influenza_US(21:end,2)),sum(Influenza_EW(12:52,2)==Influenza_EW(12:52,2)),sum(Influenza_EW(55:80,2)==Influenza_EW(55:80,2)),sum(Influenza_EW(83:119,2)==Influenza_EW(83:119,2)),sum(Influenza_EW(123:130,2)==Influenza_EW(123:130,2)),sum(Influenza_EW(133:end,2)==Influenza_EW(133:end,2))];
 
-    for n=1:14
-        Parameters(:,n) = round([(Base_Model(n).lambda) (Base_Model(n).eta_zero) (Base_Model(n).dmin) (Base_Model(n).dmax)],3);
-    end
-    
-    names = {'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield','US','England & Wales 1848','England & Wales 1890','England & Wales 1918','England & Wales 1957','England & Wales 1968'};
-    
-    for b =1:14
-        T1(:,b) = table([Parameters(:,b);Obs(b)]);
-    end
-    T1.Properties.VariableNames = names;
-    T1 = [table({ '\lambda', '\eta_{0}', 'd_{min}', 'd_{max}', 'Observations'}') T1];
-    writetable(T1,'../Figures/Table_3_v2.xlsx', 'Sheet', 'Base Model')
+for n=1:14
+    Parameters(:,n) = round([(Base_Model(n).lambda) (Base_Model(n).eta_zero) (Base_Model(n).dmin) (Base_Model(n).dmax)],3);
+end
 
-    % Model with common bounds
-    for n=1:8
-        Parameters(:,n) = round([(Common_Bounds_Model(n).lambda) (Common_Bounds_Model(n).eta_zero) (Common_Bounds_Model(n).dmin) (Common_Bounds_Model(n).dmax) ],3);
-    end
-    
-    names = { 'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield'};
-    
-    for b =1:8
-        T2(:,b) = table([Parameters(:,b);Obs(b)]);
-    end
-    T2.Properties.VariableNames = names;
-    T2 = [table({ '\lambda', '\eta_{0}', 'd_{min}', 'd_{max}', 'Observations'}') T2];
-    writetable(T2,'../Figures/Table_3_v2.xlsx', 'Sheet', 'Common Bounds Model')
-    
-    % Model with estimated bounds
-    for n=1:14
-        Parameters(:,n) = round([(Estimated_Bounds_Model(n).lambda) (Estimated_Bounds_Model(n).eta_zero) (Estimated_Bounds_Model(n).dmin) (Estimated_Bounds_Model(n).dmax) ],3);
-    end
-    
-    names = {'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield','US','England & Wales 1848','England & Wales 1890','England & Wales 1918','England & Wales 1957','England & Wales 1968'};
-    
-    for b =1:14
-        T3(:,b) = table([Parameters(:,b);Obs(b)]);
-    end
-    T3.Properties.VariableNames = names;
-    T3 = [table({ '\lambda', '\eta_{0}', 'd_{min}', 'd_{max}', 'Observations' }') T3];
-    writetable(T3,'../Figures/Table_3_v2.xlsx', 'Sheet', 'Estimated Bounds Model')
-     
+names = {'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield','US','England & Wales 1848','England & Wales 1890','England & Wales 1918','England & Wales 1957','England & Wales 1968'};
 
-    % Model with theoretical bounds
-    for n=1:14
-        Parameters(:,n) = round([(Theoretical_Bounds_Model(n).lambda) (Theoretical_Bounds_Model(n).eta_zero) (Theoretical_Bounds_Model(n).dmin) (Theoretical_Bounds_Model(n).dmax) ],3);
-    end
-    
-    names = {'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield','US','England & Wales 1848','England & Wales 1890','England & Wales 1918','England & Wales 1957','England & Wales 1968'};
-    
-    for b =1:14
-        T4(:,b) = table([Parameters(:,b);Obs(b)]);
-    end
-    T4.Properties.VariableNames = names;
-    T4 = [table({ '\lambda', '\eta_{0}', 'd_{min}', 'd_{max}', 'Observations'}') T4];
-    writetable(T4,'../Figures/Table_3_v2.xlsx', 'Sheet', 'Theoretical Bounds Model')
-    
-   % Weibull
-   Parameters =[];
-    for n=1:14
-        Parameters(:,n) = round([(Weibull_Model(n).lambda) (Weibull_Model(n).eta_zero)  ],3);
-    end
-    
-    names = {'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield','US','England & Wales 1848','England & Wales 1890','England & Wales 1918','England & Wales 1957','England & Wales 1968'};
-    
-    for b =1:14
-        T5(:,b) = table([Parameters(:,b);Obs(b)]);
-    end
-    T5.Properties.VariableNames = names;
-    T5 = [table({ '\lambda', '\eta_{0}', 'Observations'}') T5];
-    writetable(T5,'../Figures/Table_3_v2.xlsx', 'Sheet', 'Weibull Model')
+for b =1:14
+    T1(:,b) = table([Parameters(:,b);Obs(b)]);
+end
+T1.Properties.VariableNames = names;
+T1 = [table({ '\lambda', '\eta_{0}', 'd_{min}', 'd_{max}', 'Observations'}') T1];
+writetable(T1,'../Figures/Table_3_v2.xlsx', 'Sheet', 'Base Model')
+
+% Model with common bounds
+for n=1:8
+    Parameters(:,n) = round([(Common_Bounds_Model(n).lambda) (Common_Bounds_Model(n).eta_zero) (Common_Bounds_Model(n).dmin) (Common_Bounds_Model(n).dmax) ],3);
+end
+
+names = { 'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield'};
+
+for b =1:8
+    T2(:,b) = table([Parameters(:,b);Obs(b)]);
+end
+T2.Properties.VariableNames = names;
+T2 = [table({ '\lambda', '\eta_{0}', 'd_{min}', 'd_{max}', 'Observations'}') T2];
+writetable(T2,'../Figures/Table_3_v2.xlsx', 'Sheet', 'Common Bounds Model')
+
+% Model with estimated bounds
+for n=1:14
+    Parameters(:,n) = round([(Estimated_Bounds_Model(n).lambda) (Estimated_Bounds_Model(n).eta_zero) (Estimated_Bounds_Model(n).dmin) (Estimated_Bounds_Model(n).dmax) ],3);
+end
+
+names = {'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield','US','England & Wales 1848','England & Wales 1890','England & Wales 1918','England & Wales 1957','England & Wales 1968'};
+
+for b =1:14
+    T3(:,b) = table([Parameters(:,b);Obs(b)]);
+end
+T3.Properties.VariableNames = names;
+T3 = [table({ '\lambda', '\eta_{0}', 'd_{min}', 'd_{max}', 'Observations' }') T3];
+writetable(T3,'../Figures/Table_3_v2.xlsx', 'Sheet', 'Estimated Bounds Model')
+
+
+% Model with theoretical bounds
+for n=1:14
+    Parameters(:,n) = round([(Theoretical_Bounds_Model(n).lambda) (Theoretical_Bounds_Model(n).eta_zero) (Theoretical_Bounds_Model(n).dmin) (Theoretical_Bounds_Model(n).dmax) ],3);
+end
+
+names = {'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield','US','England & Wales 1848','England & Wales 1890','England & Wales 1918','England & Wales 1957','England & Wales 1968'};
+
+for b =1:14
+    T4(:,b) = table([Parameters(:,b);Obs(b)]);
+end
+T4.Properties.VariableNames = names;
+T4 = [table({ '\lambda', '\eta_{0}', 'd_{min}', 'd_{max}', 'Observations'}') T4];
+writetable(T4,'../Figures/Table_3_v2.xlsx', 'Sheet', 'Theoretical Bounds Model')
+
+% Weibull
+Parameters =[];
+for n=1:14
+    Parameters(:,n) = round([(Weibull_Model(n).lambda) (Weibull_Model(n).eta_zero)  ],3);
+end
+
+names = {'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield','US','England & Wales 1848','England & Wales 1890','England & Wales 1918','England & Wales 1957','England & Wales 1968'};
+
+for b =1:14
+    T5(:,b) = table([Parameters(:,b);Obs(b)]);
+end
+T5.Properties.VariableNames = names;
+T5 = [table({ '\lambda', '\eta_{0}', 'Observations'}') T5];
+writetable(T5,'../Figures/Table_3_v2.xlsx', 'Sheet', 'Weibull Model')
 
 %%% 8. Mortality Summary Tables
 Influenza_Cities(25,2) = 996; %% Add missing Belfast number
@@ -256,11 +256,11 @@ writetable(tttt,'../Figures/Table_2_v2.xlsx', 'Sheet', 'Range', 'Range', 'B2:G12
 writetable(t,'../Figures/Table_2_v2.xlsx', 'Sheet', 'Range', 'Range', 'A2:A12')
 
 
-    %%% Save Results        
+%%% Save Results
 
-    save('Fitted_Models_v2')
-    save('Monte_Carlo_Input_v2','Base_Model')    
-    clearvars
+save('Fitted_Models_v2')
+save('Monte_Carlo_Input_v2','Base_Model')
+clearvars
 
 
 
--- Code/Load_Data.m
+++ Code/Load_Data.m
@@ -16,29 +16,7 @@ Pneumonia = table(Belfast.Pneumonia_rate, Birmingham.Pneumonia_rate ,Cardiff.Pne
 Bronchitis = table(Belfast.Bronchitis_rate, Birmingham.Bronchitis_rate ,Cardiff.Bronchitis_rate, Glasgow.Bronchitis_rate, Liverpool.Bronchitis_rate, London.Bronchitis_rate, Manchester.Bronchitis_rate, Sheffield.Bronchitis_rate,'VariableNames', citynames);
 Bronchio_pneumonia = table(Belfast.Bronchio_pneumonia_rate, Birmingham.Bronchio_pneumonia_rate ,Cardiff.Bronchio_pneumonia_rate, Glasgow.Bronchio_pneumonia_rate, Liverpool.Bronchio_pneumonia_rate, London.Bronchio_pneumonia_rate, Manchester.Bronchio_pneumonia_rate, Sheffield.Bronchio_pneumonia_rate,'VariableNames', citynames);
 Other_respiratory = table(Belfast.Other_respiratory_rate, Birmingham.Other_respiratory_rate ,Cardiff.Other_respiratory_rate, Glasgow.Other_respiratory_rate, Liverpool.Other_respiratory_rate, London.Other_respiratory_rate, Manchester.Other_respiratory_rate, Sheffield.Other_respiratory_rate,'VariableNames', citynames);
-
-% A = [mean(Belfast.Population(26:35), 'omitnan'),mean(Birmingham.Population(26:35), 'omitnan'),mean(Cardiff.Population(26:35), 'omitnan'),mean(Glasgow.Population(26:35), 'omitnan'),mean(Liverpool.Population(26:35), 'omitnan'),mean(London.Population(26:35), 'omitnan'),mean(Manchester.Population(26:35), 'omitnan'),mean(Sheffield.Population(26:35), 'omitnan')];
-% B = [mean(Belfast.Population(36:45), 'omitnan'),mean(Birmingham.Population(36:45), 'omitnan'),mean(Cardiff.Population(36:45), 'omitnan'),mean(Glasgow.Population(36:45), 'omitnan'),mean(Liverpool.Population(36:45), 'omitnan'),mean(London.Population(36:45), 'omitnan'),mean(Manchester.Population(36:45), 'omitnan'),mean(Sheffield.Population(36:45), 'omitnan')];
-% C = [mean(Belfast.Population(46:55), 'omitnan'),mean(Birmingham.Population(46:55), 'omitnan'),mean(Cardiff.Population(46:55), 'omitnan'),mean(Glasgow.Population(46:55), 'omitnan'),mean(Liverpool.Population(46:55), 'omitnan'),mean(London.Population(46:55), 'omitnan'),mean(Manchester.Population(46:55), 'omitnan'),mean(Sheffield.Population(46:55), 'omitnan')];
-% 
-% Population = [A;B;C];
-% 
-% A = [mean(Belfast.Population_density(26:35), 'omitnan'),mean(Birmingham.Population_density(26:35), 'omitnan'),mean(Cardiff.Population_density(26:35), 'omitnan'),mean(Glasgow.Population_density(26:35), 'omitnan'),mean(Liverpool.Population_density(26:35), 'omitnan'),mean(London.Population_density(26:35), 'omitnan'),mean(Manchester.Population_density(26:35), 'omitnan'),mean(Sheffield.Population_density(26:35), 'omitnan')];
-% B = [mean(Belfast.Population_density(36:45), 'omitnan'),mean(Birmingham.Population_density(36:45), 'omitnan'),mean(Cardiff.Population_density(36:45), 'omitnan'),mean(Glasgow.Population_density(36:45), 'omitnan'),mean(Liverpool.Population_density(36:45), 'omitnan'),mean(London.Population_density(36:45), 'omitnan'),mean(Manchester.Population_density(36:45), 'omitnan'),mean(Sheffield.Population_density(36:45), 'omitnan')];
-% C = [mean(Belfast.Population_density(46:55), 'omitnan'),mean(Birmingham.Population_density(46:55), 'omitnan'),mean(Cardiff.Population_density(46:55), 'omitnan'),mean(Glasgow.Population_density(46:55), 'omitnan'),mean(Liverpool.Population_density(46:55), 'omitnan'),mean(London.Population_density(46:55), 'omitnan'),mean(Manchester.Population_density(46:55), 'omitnan'),mean(Sheffield.Population_density(46:55), 'omitnan')];
-% 
-% PopDensity = [A;B;C];
-% 
-% 
-% Tab = [Population;PopDensity];
-% VarNames = {'Population 1920s','Population 1930s','Population 1940s','Population Density 1920s','Population Density 1930s','Population Density 1940s' }';
-%     for b =1:8
-%         Tab2(:,b) = table(Tab(:,b));
-%     end
-%     Tab2.Properties.VariableNames = { 'Belfast', 'Birmingham' ,'Cardiff','Glasgow', 'Liverpool', 'London', 'Manchester', 'Sheffield'};
-%     Tab = [table(VarNames) Tab2];
-%     writetable(Tab,'../Figures/Table_2.xlsx')
-
+Infant_mortality = table(Belfast.Infant_mortality, Birmingham.Infant_mortality ,Cardiff.Infant_mortality, Glasgow.Infant_mortality, Liverpool.Infant_mortality, London.Infant_mortality, Manchester.Infant_mortality, Sheffield.Infant_mortality,'VariableNames', citynames);
 
 %% Calculate Relative Influenza & Respiratory Diseases for outbreak years
 
@@ -64,6 +42,9 @@ writematrix(Excess_Resp,'../Data/Relative_Mortality_v2.xlsx','Sheet','Respirator
 Influenza_US = table2array(readtable('../Data/National_Mortality_Data.xls', 'Sheet', ['US'], 'Range', 'A1:B57'));
 Influenza_EW = table2array(readtable('../Data/National_Mortality_Data.xls', 'Sheet', ['England & Wales'], 'Range', 'A1:B163'));
 Influenza_Cities = [years,table2array(Influenza)];
+Infant_mortality_EW =table2array(readtable('../Data/National_Mortality_Data.xls', 'Sheet', ['England & Wales'], 'Range', 'C1:C163'));
+Infant_mortality_US =table2array(readtable('../Data/National_Mortality_Data.xls', 'Sheet', ['US'], 'Range', 'C1:C57'));
+average_infant_mort = mean(table2array(Infant_mortality(1:23,:)),'omitnan')/1000;
+average_infant_mort = [average_infant_mort,mean(Infant_mortality_US,'omitnan'),mean(Infant_mortality_EW,'omitnan')];
 
-
-clearvars -except Influenza_US Influenza_EW  Influenza_Cities years citynames Pneumonia Bronchitis Resp
+clearvars -except Influenza_US Influenza_EW  Influenza_Cities years citynames Pneumonia Bronchitis Resp Infant_mortality average_infant_mort
